ba93a9a106d1841997c2d60664073abb4c51233b
Sean Igo <samwibatt@gmail.com>
2020-07-11 21:30:54 -0700

    started character aseprite n tiled map

---
+++ demo_assets/Ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.0" name="Ground" tilewidth="32" tileheight="32" tilecount="288" columns="12">
+ <image source="tiles/Ground.png" width="384" height="768"/>
+</tileset>
---
+++ demo_assets/build_assets.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# TEMPORARY ASSET BUILD FILE until I figure out how to get cmake to do its thing OR for people who don't want to use cmake to see how this works
+
+echo Building characters...
+# maybe put this in a loop and parameterize out the "Npc_Generic_019"
+aseprite -b -v --trim --inner-padding 1 --sheet outputs/Npc_Generic_019_sheet.png --sheet-columns 4 --filename-format 'c{title}*a{outertag}*d{innertag}*f{tagframe00}*p{layer}' --format json-hash --data outputs/Npc_Generic_019.json --split-layers Npc_Generic_019.aseprite
